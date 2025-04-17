78f28b73916ef9ebcc8c81fe30885fe7d39cf797
Bill Z <bill@magictavern.com>
2025-04-18 02:27:07 +0800

    Update web app and add pm2 config and script

---
+++ pm2.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [{
+        name: "time.js",
+        script: "./app.js",
+        cwd: "./",
+        log_file: "./logs/time.log",
+        restart_delay: 10000,
+        env: {
+        }
+    }]
+}
